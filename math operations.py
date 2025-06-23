5c0c62791861757aec578035f6c8d95c3b710dbc
syekumar <syekumaruppu@gmail.com>
2025-06-23 10:28:26 +0530

    Add files via upload

---
+++ math operations.py
@@ -0,0 +1,7 @@
+import math
+print("floor(4.7):", math.floor(4.7))
+print("ceil(4.1):", math.ceil(4.1))
+print("sqrt(16):", math.sqrt(16))
+print("isqrt(17):", math.isqrt(17))
+print("gcd(48, 18):", math.gcd(48, 18))
+
